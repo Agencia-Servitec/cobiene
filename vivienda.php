f4e09e60d6032b74db724414ea944fd972c72f84
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-05-24 11:20:25 -0500

    Corrección de button

--- vivienda.php
+++ vivienda.php
@@ -97,7 +97,7 @@
 
 
     <div class="collapse">
-            <div id="item-buttons_">
+            <div id="item-buttons">
 
                 <a href="#" class="button button-green button-center button-xs" id="buttonLED" onclick="mostrarTexto()">REQUISITOS PARA SER USUARIOS:</a> 
                 
